d3c39635a651250931805e161f44319e71459ecc
Santiago Pérez <santiago.perez.mt@correo.usa.edu.co>
2021-10-09 14:34:18 -0500

    Se agrega validación de información registrada en los formularios de clientes

--- js/clientes.js
+++ js/clientes.js
@@ -1,40 +1,44 @@
 function guardarcliente(){
-    console.log("ejecutando funcion para guardar");
-
     let client = {
         id: +$("#id_cliente").val(),
         name: $("#name_cliente").val(),
         email: $("#email_cliente").val(),
         age: +$("#age_cliente").val()
     };
-
-    console.log(client);
-
-    $.ajax({
-        url: url+"/ords/admin/client/client",
-        type: 'POST',
-        dataType: 'json',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify(client),
-        statusCode:{
-            201:function(){
-                alert('Se ha registrado el cliente');
+    
+    if (validarCliente(client)){
+        $.ajax({
+            url: url+"/ords/admin/client/client",
+            type: 'POST',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify(client),
+            statusCode:{
+                201:function(){
+                    alert('Se ha registrado el cliente');
+                    consultarcliente()
+                }
+            },
+            error: function (xhr, status) {
+                alert('Ya existe un cliente con ese id');
             }
-        },
-    });
+        });
+    }
+    
+    
 
 }
 
 
 function consultarcliente(){
+    $("#tabla_cliente").empty();
     $.ajax({
         url: url+ "/ords/admin/client/client",
         type: 'GET',
         dataType: 'json',
         success: function(respuesta){
-            console.log(respuesta.items);
             mostrarClientes(respuesta.items);
         },
         error: function (xhr, status) {
@@ -61,7 +65,7 @@ function mostrarClientes(items){
                    <td>${items[i].email}</td>
                    <td>${items[i].age}</td>
                    <td>
-                        <button onclick="">Eliminar</button>
+                        <button onclick="eliminarCliente(${items[i].id})">Eliminar</button>
                         <a href="detalleCliente.html?id=${items[i].id}">Ver detalle</a>
                    </td> 
                 </tr>`;
@@ -88,5 +92,69 @@ function consultarClientePorId(id){
 }
 
 function mostrarClienteUnico(item) {
-        
+    $("#id_cliente").val(item.id);
+    $("#name_cliente").val(item.name);
+    $("#email_cliente").val(item.email);
+    $("#age_cliente").val(item.age);
+}
+
+
+function actualizarCliente(){
+
+    let client = {
+        id: +$("#id_cliente").val(),
+        name: $("#name_cliente").val(),
+        email: $("#email_cliente").val(),
+        age: +$("#age_cliente").val()
+    };
+
+    console.log(client);
+    if (validarCliente(client)){
+        $.ajax({
+            url: url+"/ords/admin/client/client",
+            type: 'PUT',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify(client),
+            statusCode:{
+                201:function(){
+                    alert('Se han actualizado los datos del cliente');
+                    window.location.assign('index.html')
+                }
+            },
+        });
+
+        }
+}
+
+function eliminarCliente(id){
+    let opc = confirm('¿Está seguro que desea eliminar a ese cliente?')
+    if(opc){
+        $.ajax({
+            url: url+"/ords/admin/client/client",
+            type: 'DELETE',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({id:id}),
+            statusCode:{
+                204:function(){
+                    alert('Se ha eliminado el cliente');
+                    consultarcliente()
+                }
+            },
+        });
+    }
+
+}
+
+function validarCliente(client){
+    if (client.id<=0|| client.name===''|| client.email===''|| client.age<=0){
+        alert("Procure no dejar campos vacíos\nEl id y la edad son números no negativos")
+        return false;
+    }
+    return true;
 }
