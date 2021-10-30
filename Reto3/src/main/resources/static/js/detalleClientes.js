3d5f65f9bb2e3ef45ed82a4598a729480aa89602
Santiago Pérez <santiago.perez.mt@correo.usa.edu.co>
2021-10-30 13:05:21 -0500

    Se agrega edición de clientes

--- Reto3/src/main/resources/static/js/detalleClientes.js
+++ Reto3/src/main/resources/static/js/detalleClientes.js
@@ -8,22 +8,4 @@ $(document).ready(function () {
     }
 });
 
-function consultarClientePorId(id){
-    $.ajax({
-        url: url+"/api/Client/"+id,
-        type: 'GET',
-        dataType: 'json',
-        success: function(respuesta){
-            console.log(respuesta.items);
-            if (respuesta.items.length==1){
-                llenarDatos(respuesta.items[0]);
-            }else{
-                $("#boton").hide();
-                alert('No se encuentra el cliente con el id '+id);
-            }
-        },
-        error: function (xhr, status) {
-            alert('ha sucedido un problema');
-        }
-    });
-}
+
